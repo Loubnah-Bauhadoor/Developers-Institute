bb7ba2749832a2bbdf7d15a6209c53662b8b6d2d
Loubnah <loubnahbauhadoor@gmail.com>
2022-11-15 10:17:15 +0100

    Day 2

---
+++ Day2/javascript/main2.js
@@ -0,0 +1,77 @@
+/* let x = 3 + 7
+console.log(x) */
+
+
+
+
+// let x = 5
+
+
+// here we run the js code
+
+
+/*
+save as
+.js
+.css
+.html
+
+*/  
+
+
+
+
+/* let name;
+undefined
+
+let y = 3 + 7;
+let c = 5;
+
+let myStr = "aaa" */
+
+
+//Exercise 1
+
+let addressNumber;
+let addressStreet;
+let country;
+
+addressNumber = 6;
+addressStreet = "Rose-Belle";
+country = "Mauritius";
+
+let globalAddress = " I live in " + addressStreet + " " + addressNumber + ", in " + country;
+
+
+
+
+
+//Exercise 2
+//birthdate
+
+/* or
+let birthdate = 1999;
+let futureYear = 2023;
+
+*/
+
+
+let birthdate;
+let futureYear;
+
+
+birthdate = 1999
+futureYear = 2023
+
+
+
+let globalYear = "i will be " + ( futureYear -  birthdate ) + " years old in " + futureYear
+
+
+//Exercise 3
+
+
+// 
+
+|| (or)
+or |
